87793a43ed903a7118c5ab208eb3ce104ecf5c2d
Jason Axelson <jason.axelson@gmail.com>
2023-09-03 14:30:29 -1000

    Initial Commit

---
+++ lib/assets.ex
@@ -0,0 +1,7 @@
+defmodule HelloScenicFull.Assets do
+  use Scenic.Assets.Static,
+    otp_app: :hello_scenic_full,
+    alias: [
+      parrot: "images/cyanoramphus_zealandicus_1849.jpg"
+    ]
+end
---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule HelloScenicFull.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :hello_scenic_full,
+      version: "0.1.0",
+      elixir: "~> 1.9",
+      build_embedded: true,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      mod: {HelloScenicFull, []},
+      extra_applications: []
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:scenic, "~> 0.11.0"},
+      {:scenic_driver_local, "~> 0.11.0"},
+      {:scenic_clock, "~> 0.11.0"}
+    ]
+  end
+end
